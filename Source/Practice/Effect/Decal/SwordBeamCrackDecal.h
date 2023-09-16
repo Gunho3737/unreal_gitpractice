cafc091700273ab7ae096fc0514a150a87333db2
vkekrahs3737@gmail.com <vkekrahs3737@gmail.com>
2023-09-16 19:46:37 +0900

    23.09.16 소드빔임팩트데칼 추가

---
+++ Source/Practice/Effect/Decal/SwordBeamCrackDecal.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DecalActor.h"
+#include "SwordBeamCrackDecal.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PRACTICE_API ASwordBeamCrackDecal : public ADecalActor
+{
+	GENERATED_BODY()
+	
+};
