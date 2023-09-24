1bf9de3f9ada38ce1bebdf5753237abc38d98afe
vkekrahs3737@gmail.com <vkekrahs3737@gmail.com>
2023-09-24 17:01:57 +0900

    23.09.24 플레이어 인포 UI 연동

--- Source/Practice/UI/PlayerInfoWidget.cpp
+++ Source/Practice/UI/PlayerInfoWidget.cpp
@@ -3,3 +3,36 @@
 
 #include "PlayerInfoWidget.h"
 
+#include "Components/Image.h"
+#include "Components/ProgressBar.h"
+#include "Components/TextBlock.h"
+
+void UPlayerInfoWidget::SetName(FString _Name)
+{
+	m_Name->SetText(FText::FromString(_Name));
+}
+
+void UPlayerInfoWidget::SetHPBarRatio(float _ratio)
+{
+	m_HPBar->SetPercent(_ratio);
+}
+
+void UPlayerInfoWidget::SetMPBarRatio(float _ratio)
+{
+	m_MPBar->SetPercent(_ratio);
+}
+
+void UPlayerInfoWidget::NativeConstruct()
+{
+	Super::NativeConstruct();
+
+	m_Name = Cast<UTextBlock>(GetWidgetFromName(FName("CharacterName")));
+	m_Image = Cast<UImage>(GetWidgetFromName(FName("Portrait")));
+	m_HPBar = Cast<UProgressBar>(GetWidgetFromName(FName("HPBar")));
+	m_MPBar = Cast<UProgressBar>(GetWidgetFromName(FName("MPBar")));
+}
+
+void UPlayerInfoWidget::NativeTick(const FGeometry& _geo, float _DT)
+{
+	Super::NativeTick(_geo, _DT);
+}
--- Source/Practice/UI/PlayerInfoWidget.h
+++ Source/Practice/UI/PlayerInfoWidget.h
@@ -14,4 +14,21 @@ class PRACTICE_API UPlayerInfoWidget : public UUserWidget
 {
 	GENERATED_BODY()
 	
+private:
+	//UMG의 컴포넌트들을 받아오기 위한 클래스
+	class UTextBlock* m_Name;
+	class UImage* m_Image;
+	class UProgressBar* m_HPBar;
+	class UProgressBar* m_MPBar;
+
+
+public:
+	void SetName(FString _Name);
+	void SetHPBarRatio(float _ratio);
+	void SetMPBarRatio(float _ratio);
+
+public:
+	virtual void NativeConstruct() override;
+	virtual void NativeTick(const FGeometry& _geo, float _DT) override;
+
 };
