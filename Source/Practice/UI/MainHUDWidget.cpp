d0070ad0ba269c875c0a5fc0fc57c57e3b526734
vkekrahs3737@gmail.com <vkekrahs3737@gmail.com>
2023-09-26 22:29:34 +0900

    23.09.26 메인 HUD위젯 내용 추가, 메인UHD에 인벤토리 추가

--- Source/Practice/UI/MainHUDWidget.cpp
+++ Source/Practice/UI/MainHUDWidget.cpp
@@ -3,3 +3,58 @@
 
 #include "MainHUDWidget.h"
 
+void UMainHUDWidget::ShowInventoryUI(bool _bShow)
+{
+	//인벤토리를 보이지 않도록 함
+	if (_bShow)
+	{
+		m_Inventory->SetVisibility(ESlateVisibility::Visible);
+	}
+	else
+	{
+		m_Inventory->SetVisibility(ESlateVisibility::Hidden);
+	}
+}
+
+bool UMainHUDWidget::IsInventoryOpen()
+{
+	if (ESlateVisibility::Visible == m_Inventory->GetVisibility())
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+
+void UMainHUDWidget::SetPlayerHPRatio(float _Ratio)
+{
+	m_PlayerInfo->SetHPBarRatio(_Ratio);
+}
+
+void UMainHUDWidget::NativeConstruct()
+{
+	Super::NativeConstruct();
+
+	m_PlayerInfo = Cast<UPlayerInfoWidget>(GetWidgetFromName(FName("PlayerInfo")));
+	if (!IsValid(m_PlayerInfo))
+	{
+		LOG(LogTemp, Error, TEXT("PlayerInfo 못 찾음"));
+	}
+
+	m_Inventory = Cast<UInventory_Base>(GetWidgetFromName(FName("Inventory")));
+	if (!IsValid(m_Inventory))
+	{
+		LOG(LogTemp, Error, TEXT("Inventory 위젯 못 찾음"));
+	}
+	else
+	{
+		m_Inventory->SetVisibility(ESlateVisibility::Hidden);
+	}
+}
+
+void UMainHUDWidget::NativeTick(const FGeometry& _geo, float _DT)
+{
+	Super::NativeTick(_geo, _DT);
+}
--- Source/Practice/UI/MainHUDWidget.h
+++ Source/Practice/UI/MainHUDWidget.h
@@ -2,8 +2,12 @@
 
 #pragma once
 
-#include "CoreMinimal.h"
+#include "../Header/GlobalHeader.h"
 #include "Blueprint/UserWidget.h"
+#include "Inventory_Base.h"
+#include "PlayerInfoWidget.h"
+
+#include "CoreMinimal.h"
 #include "MainHUDWidget.generated.h"
 
 /**
@@ -13,5 +17,23 @@ UCLASS()
 class PRACTICE_API UMainHUDWidget : public UUserWidget
 {
 	GENERATED_BODY()
+
+private:
+	UPlayerInfoWidget* m_PlayerInfo;
+	UInventory_Base* m_Inventory;
+
+public:
+	UPlayerInfoWidget* GetPlayerInfoWidget() { return m_PlayerInfo; }
+	UInventory_Base* GetInventoryWidget() { return m_Inventory; }
+
+	void ShowInventoryUI(bool _bShow);
+	bool IsInventoryOpen();
+
+
+	void SetPlayerHPRatio(float _Ratio);
+
+public:
+	virtual void NativeConstruct() override;
+	virtual void NativeTick(const FGeometry& _geo, float _DT) override;
 	
 };
