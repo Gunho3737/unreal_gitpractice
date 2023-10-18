93f7c0d1a60a9fefc7e4f6405cad63d0852ce80d
[o0ojenny] <[o0o_jenny@naver.com]>
2023-10-18 12:12:46 +0900

    23.10.18 랜드스케이프에 사용할 피지컬 머티리얼 생성

---
+++ Source/Practice/Material/PhysicMT/MyLandScapePhysicalMaterial.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PhysicalMaterials/PhysicalMaterial.h"
+#include "MyLandScapePhysicalMaterial.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PRACTICE_API UMyLandScapePhysicalMaterial : public UPhysicalMaterial
+{
+	GENERATED_BODY()
+	
+};
