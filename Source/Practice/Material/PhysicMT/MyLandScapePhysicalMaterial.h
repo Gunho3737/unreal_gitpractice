6684c7683f098f14b3df1156efa274b98d4dfba5
[o0ojenny] <[o0o_jenny@naver.com]>
2023-10-18 14:40:15 +0900

    23.10.18 피직스 머티리얼 프로퍼티 추가

--- Source/Practice/Material/PhysicMT/MyLandScapePhysicalMaterial.h
+++ Source/Practice/Material/PhysicMT/MyLandScapePhysicalMaterial.h
@@ -2,6 +2,7 @@
 
 #pragma once
 
+#include "../../Header/GlobalHeader.h"
 #include "CoreMinimal.h"
 #include "PhysicalMaterials/PhysicalMaterial.h"
 #include "MyLandScapePhysicalMaterial.generated.h"
@@ -14,4 +15,22 @@ class PRACTICE_API UMyLandScapePhysicalMaterial : public UPhysicalMaterial
 {
 	GENERATED_BODY()
 	
+private:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (AllowPrivateAccess = "true"))
+	USoundBase* m_Sound;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (AllowPrivateAccess = "true"))
+	UParticleSystem* m_Particle;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (AllowPrivateAccess = "true"))
+	UNiagaraSystem* m_Niagara;
+
+public:
+	void SetSound(USoundBase* _Sound) { m_Sound = _Sound; }
+	void SetParticle(UParticleSystem* _Particle) { m_Particle = _Particle; }
+	void SetNiagara(UNiagaraSystem* _Niagara) { m_Niagara = _Niagara; }
+
+	USoundBase* GetSound() { return m_Sound; }
+	UParticleSystem* GetParticle() { return m_Particle; }
+	UNiagaraSystem* GetNiagara() { return m_Niagara; }
 };
