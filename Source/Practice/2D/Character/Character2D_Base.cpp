cc1363efb63c5df6ba4fb881e4f067bd28992583
[o0ojenny] <[o0o_jenny@naver.com]>
2023-10-23 16:46:33 +0900

    23.10.23 2D캐릭터 기초 재생 세팅, 블루프린트 내부 플립북세팅

--- Source/Practice/2D/Character/Character2D_Base.cpp
+++ Source/Practice/2D/Character/Character2D_Base.cpp
@@ -3,3 +3,97 @@
 
 #include "Character2D_Base.h"
 
+//플립북 컴포넌트
+#include "PaperFlipbookComponent.h"
+
+bool ACharacter2D_Base::ChangeState(EPLAYER2D_STATE _NextState)
+{
+	//현재스테이트와 다음 스테이트가 같으면 아무것도 안하고 리턴
+	if (CurState == _NextState)
+		return false;
+
+	CurState = _NextState;
+
+	switch (CurState)
+	{
+	case EPLAYER2D_STATE::IDLE:
+		break;
+	case EPLAYER2D_STATE::MOVE:
+		break;
+	case EPLAYER2D_STATE::JUMP:
+		break;
+	case EPLAYER2D_STATE::ATTACK:
+		break;
+	case EPLAYER2D_STATE::FALLDOWN:
+		break;
+	case EPLAYER2D_STATE::GETDAMAGE:
+		break;
+	case EPLAYER2D_STATE::DEAD:
+		break;
+	case EPLAYER2D_STATE::NONE:
+		break;
+	default:
+		break;
+	}
+
+	//현재 상태에 맞춘 플립북을 재생
+	PlayFlipbook(CurState, CurDir);
+
+	return false;
+}
+
+void ACharacter2D_Base::PlayFlipbook(EPLAYER2D_STATE _CurState, EPLAYER2D_DIR _CurDir, bool _bLoop, int32 _StartFrame)
+{
+	//Array전체를 돌면서
+	for (int32 i = 0; i < FlipbookInfoArray.Num(); ++i)
+	{
+		//내 현재 state와 현재 dir와 같은 세팅을 가진 flipbook을 찾았을시
+		if (_CurState == FlipbookInfoArray[i].State && _CurDir == FlipbookInfoArray[i].Dir)
+		{
+			//그 Array에 플립북 정보가 들어가 있으면
+			if (IsValid(FlipbookInfoArray[i].Flipbook))
+			{
+				//스프라이트를 찾아낸 플립북으로 세팅하고
+				GetSprite()->SetFlipbook(FlipbookInfoArray[i].Flipbook);
+				//x번째 프레임부터 재생하며
+				GetSprite()->SetPlaybackPositionInFrames(_StartFrame, false);
+				//루프 여부를 세팅한다음
+				GetSprite()->SetLooping(_bLoop);
+				//플립북을 재생
+				GetSprite()->Play();
+			}
+
+			return;
+		}
+	}
+}
+
+void ACharacter2D_Base::BeginPlay()
+{
+	Super::BeginPlay();
+
+	CurDir = EPLAYER2D_DIR::RIGHT;
+	ChangeState(EPLAYER2D_STATE::IDLE);
+}
+
+void ACharacter2D_Base::Tick(float _DT)
+{
+	Super::Tick(_DT);
+}
+
+void ACharacter2D_Base::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+}
+
+ACharacter2D_Base::ACharacter2D_Base()
+	: CurState(EPLAYER2D_STATE::NONE), CurDir(EPLAYER2D_DIR::RIGHT)
+{
+	//카메라 컴포넌트를 Root에 부착
+	Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("CameraComponent"));
+	Camera->SetupAttachment(GetRootComponent());
+}
+
+ACharacter2D_Base::~ACharacter2D_Base()
+{
+}
--- Source/Practice/2D/Character/Character2D_Base.h
+++ Source/Practice/2D/Character/Character2D_Base.h
@@ -4,14 +4,93 @@
 
 #include "CoreMinimal.h"
 #include "PaperCharacter.h"
+#include "PaperFlipbook.h"
+#include "../../Header/GlobalHeader.h"
+
 #include "Character2D_Base.generated.h"
 
 /**
  * 
  */
+
+//2D캐릭터의 상태를 나타냄
+UENUM(BlueprintType)
+enum class EPLAYER2D_STATE : uint8
+{
+	IDLE,
+	MOVE,
+	JUMP,
+	ATTACK,
+	FALLDOWN,
+	GETDAMAGE,
+	DEAD,
+	NONE,
+};
+
+UENUM(BlueprintType)
+enum class EPLAYER2D_DIR : uint8
+{
+	LEFT,
+	RIGHT,
+};
+
+//array안에 들어갈 flipbook들의 정보가 담긴 구조체
+USTRUCT(Atomic, BlueprintType)
+struct FFlipbookInfo
+{
+	GENERATED_BODY()
+
+public:
+	//무슨 스테이트 상에서 실행될 것인지
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (AllowPrivateAccess = "true"))
+	EPLAYER2D_STATE		State;
+	//플레이어의 방향이 좌방향인지 우방향 인지
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (AllowPrivateAccess = "true"))
+	EPLAYER2D_DIR		Dir;  
+
+	//플레이어의 State,Dir 이 겹쳣을때 실행시킬 FlipBookComponent
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (AllowPrivateAccess = "true"))
+	UPaperFlipbook* Flipbook;
+};
+
 UCLASS()
 class PRACTICE_API ACharacter2D_Base : public APaperCharacter
 {
 	GENERATED_BODY()
+
+public:
+	//캐릭터가 사용할 플립북을 모아둘 TArray
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Info, meta = (AllowPrivateAccess = "true"))
+	TArray<FFlipbookInfo> FlipbookInfoArray;
 	
+	//플레이어의 현재 방향
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Info, meta = (AllowPrivateAccess = "true"))
+	EPLAYER2D_DIR CurDir;
+	
+	//플레이어의 현재 상태
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Info, meta = (AllowPrivateAccess = "true"))
+	EPLAYER2D_STATE			CurState;
+
+	//카메라 컴포넌트
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Component, meta = (AllowPrivateAccess = "true"))
+	UCameraComponent* Camera;
+
+public:
+	UCameraComponent* GetCamera() { return Camera; }
+
+	//현재 상태를 전환하기 위한 함수
+	bool ChangeState(EPLAYER2D_STATE _NextState);
+	
+	//상태와 방향을 넣어주면, 그거에 맞춘 플립북을 재생하는 함수
+	void PlayFlipbook(EPLAYER2D_STATE _CurState, EPLAYER2D_DIR _CurDir, bool _bLoop = true, int32 _StartFrame = 0);
+
+public:
+
+	virtual void BeginPlay() override;
+	virtual void Tick(float _DT) override;
+	//인풋 바인딩을 위한 함수
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	ACharacter2D_Base();
+	virtual ~ACharacter2D_Base();
 };
