a26da457aa62b6c423843a01b61bc1273863c390
[o0ojenny] <[o0o_jenny@naver.com]>
2023-10-23 14:08:59 +0900

    23.10.23 커스텀 PaperCharacter 생성, 2D월드에 배치

---
+++ Source/Practice/2D/Character/Character2D_Base.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PaperCharacter.h"
+#include "Character2D_Base.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PRACTICE_API ACharacter2D_Base : public APaperCharacter
+{
+	GENERATED_BODY()
+	
+};
