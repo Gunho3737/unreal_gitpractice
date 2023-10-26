1b01a030f78de656625f0284710a0221037f4361
[o0ojenny] <[o0o_jenny@naver.com]>
2023-10-26 15:43:22 +0900

    23.10.26 fpsplayer 애님인스턴스 c++클래스화

---
+++ Source/Practice/FPSPopol/Player/FPSPlayerAnimInstance.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "FPSPlayerAnimInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PRACTICE_API UFPSPlayerAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+	
+};
