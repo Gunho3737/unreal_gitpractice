bc108364473cbf4e9d1b25ca5ee34609cf3913bd
[o0ojenny] <[o0o_jenny@naver.com]>
2023-10-31 15:52:46 +0900

    23.10.31 FPS 메인위젯, 플레이어정보 위젯 추가

---
+++ Source/Practice/FPSPopol/UI/PlayerUI/FPS_MainWidget.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "FPS_MainWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PRACTICE_API UFPS_MainWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/Practice/FPSPopol/UI/PlayerUI/FPS_PlayerInfoWidget.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "FPS_PlayerInfoWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PRACTICE_API UFPS_PlayerInfoWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
