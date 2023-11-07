6ab2ec98db5f72b9467279d52b278b63ba8a42fd
[o0ojenny] <[o0o_jenny@naver.com]>
2023-11-07 11:26:52 +0900

    23.11.07 몬스터공격시 플레이어위치 바라보도록 세팅

--- Source/Practice/Character/Monster/Melee_Monster/BTTMonAttack.cpp
+++ Source/Practice/Character/Monster/Melee_Monster/BTTMonAttack.cpp
@@ -5,6 +5,7 @@
 #include "../Monster_Base.h"
 #include "BehaviorTree/BlackboardComponent.h"
 #include "AIController.h"
+#include "Kismet/KismetMathLibrary.h"
 #include "../../../FPSPopol/Player/FPSPlayer.h"
 
 UBTTMonAttack::UBTTMonAttack()
@@ -23,6 +24,7 @@ EBTNodeResult::Type UBTTMonAttack::ExecuteTask(UBehaviorTreeComponent& OwnerComp
 		return EBTNodeResult::Failed;
 	}
 
+
 	if (true == OwnerComp.GetBlackboardComponent()->GetValueAsBool(FName("IsAttackAnimationPlay")))
 	{
 		//애니메이션이 이미 실행중이면 작업생략하지만 다른노드로 넘어가면 안되므로 succeeded로 넘겨줌
@@ -30,24 +32,22 @@ EBTNodeResult::Type UBTTMonAttack::ExecuteTask(UBehaviorTreeComponent& OwnerComp
 	}
 
 	//몬스터를 플레이어 방향으로 회전시키기
-	UObject* pObject = OwnerComp.GetBlackboardComponent()->GetValueAsObject(FName("TraceTarget"));
+	UObject* pObject = OwnerComp.GetBlackboardComponent()->GetValueAsObject(FName("Target"));
 	if (IsValid(pObject))
 	{
 		AActor* pTarget = Cast<AActor>(pObject);
 		FVector PlayerPos = pTarget->GetActorLocation();
-		//플레이어 위치로 회전
-		//지금 적용 안되는중 다른거 쓰기
-		pMonster->GetActorRotation().RotateVector(PlayerPos);
+		FRotator MonLookAt = UKismetMathLibrary::FindLookAtRotation(pMonster->GetActorLocation(), PlayerPos);
+		pMonster->SetActorRotation(MonLookAt);
 	}
 
-	
 	float Speed = pMonster->GetMovementComponent()->Velocity.Size();
-
 	if (Speed >= 0.f)
 	{
 		//VeloCity정지시키기
 		pMonster->GetMovementComponent()->Velocity = { 0.f,0.f,0.f };
 	}
+
 
 	switch (pMonster->GetMonsterType())
 	{
